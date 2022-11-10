3eace43b858ea1f9e83571ade6b9c02db27c0c77
takumi <t.nakamura.sys22@morijyobi.ac.jp>
2022-11-10 10:32:07 +0900

    ensyu

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.print("・身長(m)を入力してください：");
+        double height = stdIn.nextDouble();
+        System.out.print("・体重(Kg)を入力してください：");
+        double weight = stdIn.nextDouble();
+        double bmi = weight / Math.pow(height, 2);
+        System.out.println("BMI値は" + (Math.floor(bmi*100)/100) + "です。");
+      }
+    }
+
